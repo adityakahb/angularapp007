007b34b4ec2d0c4474e63541d0bb97f596bb4d34
adityakahb <aditya.bhakay@live.com>
2020-08-12 13:13:10 +0530

    updated to angular 10

--- client/README.md
+++ client/README.md
@@ -1,6 +1,6 @@
-# Ecommapp001
+# Client
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.5.
 
 ## Development server
 
--- client/src/main.ts
+++ client/src/main.ts
@@ -8,35 +8,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-// const loadCSSFiles = (url) => {
-//   const link = document.createElement('link');
-//   link.setAttribute('rel', 'stylesheet');
-//   link.setAttribute('href', url);
-//   document.head.appendChild(link);
-// };
-
-// const loadJSFiles = (url) => {
-//   return new Promise((resolve, reject) => {
-//     const script = document.createElement('script');
-//     script.src = url;
-
-//     script.addEventListener('load', () => {
-//       // The script is loaded completely
-//       resolve(true);
-//     });
-
-//     document.body.appendChild(script);
-//   });
-// };
-
-// if (window && document) {
-//   loadCSSFiles('http://localhost:4100/fontsbundle');
-//   loadCSSFiles('http://localhost:4100/cssbundle');
-//   loadJSFiles('http://localhost:4100/jsbundle').then(() => {
-//     document.addEventListener('DOMContentLoaded', () => {
-//       platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
-//     });
-//   });
-// }
-
-platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
